4e94ede911490a3446af25b9c3907b5cdb1810b3
remi.odufuye <remi.odufuye@gmail.com>
2019-10-15 17:35:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -59,7 +59,7 @@ monopoly =
            :names => {  ### fourth tier here 
                         :reading_railroad => {"mortgage_value"=>"$100"} ,
                         :pennsylvania_railroad => { "mortgage_value"=>"$200"} ,
-                        :b_and_o_railroad => {} ,
+                        :b_and_o_railroad => {  "mortgage_value"=>"$400"} ,
                         :shortline_railroad => {}
                      }
            
